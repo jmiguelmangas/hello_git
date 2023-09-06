6577d3e8598e4c11e2e3db95f5f9946897c4fc81
jmiguelmangas <jmmangas@gmail.com>
2023-09-06 17:20:14 +0200

    Añadido eres un cascarrabias

--- hellogit.py
+++ hellogit.py
@@ -12,6 +12,6 @@ def main():
     if check_question(question):
         print(f"Hello Git, today is {today_is()} and your question is: {question}")
     say_bye()
-
+        print("Eres un cascarrabias")
 
 main()
