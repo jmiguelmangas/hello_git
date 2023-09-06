84b36ec77f9dfc175935a801653b876a13e2435b
jmiguelmangas <jmmangas@gmail.com>
2023-09-06 16:28:09 +0200

    Implementacion Basica de Login

---
+++ login.py
@@ -0,0 +1,33 @@
+import sys
+
+userdata = [
+    {"username": "pepito", "password": "123456"},
+    {"username": "juanito", "password": "665544"},
+]
+
+
+def ask_username_password():
+    username = input("Username: ").lower()
+    password = input("Password: ").lower()
+    return (username, password)
+
+
+def check_user_password(login):
+    username, password = login
+    for user in userdata:
+        if user["username"] == username:
+            if user["password"] == password:
+                return True
+            else:
+                sys.exit("Incorrect Password")
+    sys.exit("Incorrect User")
+
+
+def main():
+    login = ask_username_password()
+    if check_user_password(login):
+        print(f"Hello {login[0].capitalize()} welcome to the system")
+
+
+if __name__ == "__main__":
+    main()
