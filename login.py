7081e22706c2f28409c69c7f61152c8849d4ee2b
jmiguelmangas <jmmangas@gmail.com>
2023-09-07 12:09:45 +0200

    Login V2

--- login.py
+++ login.py
@@ -24,6 +24,7 @@ def check_user_password(login):
 
 
 def main():
+    print("Login V2")
     login = ask_username_password()
     if check_user_password(login):
         print(f"Hello {login[0].capitalize()} welcome to the system")
