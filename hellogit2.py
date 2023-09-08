6db5887c09a65bf60776542716e7de9614697f1f
Jose Miguel Mangas <48366896+jmiguelmangas@users.noreply.github.com>
2023-09-08 13:59:24 +0200

    Update hellogit2.py

--- hellogit2.py
+++ hellogit2.py
@@ -1,3 +1,3 @@
 print("This is my new git v2")
 print("It only shows that new version for viewing purposes")
-print("It only shows that new version for viewing purposes 3")
+print("It only shows that new version for viewing purposes for real")
